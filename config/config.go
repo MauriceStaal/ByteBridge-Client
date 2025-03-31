97aef739e376a6a36526a1a298176a71a5b16dfb
Erwin <steennn@gmail.com>
2025-03-31 15:29:23 +0200

    Update ByteBridge TCP URL.

--- config/config.go
+++ config/config.go
@@ -22,7 +22,7 @@ var InfoLogger *log.Logger
 func Initialize() {
 	Config = Configuration{
 		APIBaseURL: "https://bytebridge.es8.nl/api/v1",
-		TCPURL:     "bytebridge-tcp.es8.nl:443",
+		TCPURL:     "bytebridge.es8.nl:50500",
 		Debug:      false,
 	}
 
